14be51f8bcc16503552c4cff7679d01a9ee302ce
Juan Carlos <juancagb.17@gmail.com>
2017-12-09 11:55:20 -0500

    Fix seeder based on the new field username

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -14,6 +14,7 @@ class UsersTableSeeder extends Seeder
     {
         User::create([
         	'name' => 'Juan',
+            'username' => 'jcarlos',
             'email' => 'juancagb.17@gmail.com',
             'password' => bcrypt('123123'),
             'admin' => true
